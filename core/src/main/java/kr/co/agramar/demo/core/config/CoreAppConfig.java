6186c59ebf4ebc0b0c7bc536e04ff91db9fbb014
agramar <iamsjh1017@gmail.com>
2021-05-12 09:37:02 +0900

    initial commit

---
+++ core/src/main/java/kr/co/agramar/demo/core/config/CoreAppConfig.java
@@ -0,0 +1,8 @@
+package kr.co.agramar.demo.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CoreAppConfig {
+
+}
---
+++ core/src/test/java/kr/co/agramar/demo/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.agramar.demo.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class CoreApplicationTests {
+
+    @Test
+    void test() {
+    	log.debug("test");
+    }
+}
